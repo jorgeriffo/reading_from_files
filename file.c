7065400f2aa8831a7abf80a0a9e07adb550f0d0c
jorgeriffo <jorgeriffocl@gmail.com>
2017-04-14 22:17:36 -0400

    Add files via upload

---
+++ file.c
@@ -0,0 +1,31 @@
+
+//UNDERTANDING FILES IN C
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+
+		FILE *fp = NULL;
+		int sku = 4664;
+		double price;
+
+		fp = fopen("alpha.txt", "r");
+		if (fp != NULL) {
+       
+       fscanf(fp, "%d %lf", &sku, &price);
+
+       fprintf(fp, "sku = %d price = %10.2lf\n", sku, price);
+       fclose(fp);
+
+   } else {
+
+   	 printf("Failed to open file\n");
+
+   }
+
+     return 0; 
+}
